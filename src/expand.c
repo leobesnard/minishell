db3e6c2e60af0cf19a75afa721a11d14b6b6588e
Leo BESNARD <lbesnard@e1r7p11.clusters.42paris.fr>
2022-08-29 19:35:27 +0200

    fini

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: rmorel <rmorel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 14:56:45 by rmorel            #+#    #+#             */
-/*   Updated: 2022/08/29 16:17:48 by lbesnard         ###   ########.fr       */
+/*   Updated: 2022/08/29 18:39:41 by lbesnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	test_check_quotes(char *str)
 	int	i;
 
 	i = 0;
+	if (!str)
+		return (-1);
 	while (str[i])
 	{
 		if (str[i] && str[i] == '\'')
@@ -138,12 +140,13 @@ char	*expand(t_list *env, char *str)
 					if (str[var.i + 1] && ft_is_var_char(str[var.i + 1]))
 					{
 						var.i++;
-						var.str = var_str(&str[var.i])
-						var.var = find_env_var(env, var_str(&str[var.i]));
+						var.str = var_str(&str[var.i]);
+						var.var = find_env_var(env, var.str);
 						if (var.var)
 							ft_strlcpy(&var.ret[var.u], var.var, ft_strlen(var.var) + 1);
 						while (ft_is_var_char(str[var.i]))
 							var.i++;
+						free(var.str);
 						if (var.var)
 							var.u += ft_strlen(var.var);
 					}
@@ -168,11 +171,13 @@ char	*expand(t_list *env, char *str)
 			if (str[var.i + 1] && ft_is_var_char(str[var.i + 1]))
 			{
 				var.i++;
-				var.var = find_env_var(env, var_str(&str[var.i]));
+				var.str = var_str(&str[var.i]);
+				var.var = find_env_var(env, var.str);
 				if (var.var)
 					ft_strlcpy(&var.ret[var.u], var.var, ft_strlen(var.var) + 1);
 				while (ft_is_var_char(str[var.i]))
 					var.i++;
+				free(var.str);
 				if (var.var)
 					var.u += ft_strlen(var.var);
 			}
